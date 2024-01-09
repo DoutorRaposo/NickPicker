b446795cd680e753c1a322588f8b46a858c2d6db
DoutorRaposo <rafaelvga93@gmail.com>
2024-01-09 20:06:12 -0300

    Script for genre created.

--- db_script/genre_script.py
+++
@@ -1,8 +0,0 @@
-import json
-from nick.models import *
-
-def script():
-    with open("genres_movie.json") as file:
-        data = json.load(file)
-        for genre in data["genres"]:
-            Genre.objects.create(name=genre['name'], tmdb_id=genre['id'])
