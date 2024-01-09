6907aa94afa7332ec4dc80198cb5392f6239ee1d
DoutorRaposo <rafaelvga93@gmail.com>
2024-01-09 20:02:15 -0300

    Genre database

--- movie_picker/nick/admin.py
+++ movie_picker/nick/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Title, Genre
 
-admin.site.register(Movie)
+admin.site.register(Title)
+admin.site.register(Genre)
--- movie_picker/nick/models.py
+++ movie_picker/nick/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
 
-class Movie(models.Model):
+class Title(models.Model):
     title = models.CharField(max_length=64)
+    overview = models.TextField(blank=True)
+    release_date = models.DateTimeField(null=True)
+    tmdb_id = models.IntegerField(null=True)
+    genre = models.ManyToManyField("Genre", related_name="genres", blank=True)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=32)
+    tmdb_id = models.IntegerField()
+
+    def __str__(self):
+        return self.name
