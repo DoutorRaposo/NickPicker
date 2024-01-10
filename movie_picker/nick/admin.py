7ac888cfcf84e7f81c908b6a0a4763d27dece9fe
DoutorRaposo <rafaelvga93@gmail.com>
2024-01-09 23:18:53 -0300

    Customized Admin View

--- movie_picker/nick/admin.py
+++ movie_picker/nick/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Title, Genre
 
-admin.site.register(Title)
-admin.site.register(Genre)
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "release_date", "role", "media_type", "credit_type", "get_genres")
+    list_filter = ("credit_type", "media_type")
+    search_fields = ("title", "id", "media_type")
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
+    filter_horizontal = ("movies",)
+
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Genre, GenreAdmin)
--- movie_picker/nick/models.py
+++ movie_picker/nick/models.py
@@ -16,7 +16,7 @@ class Title(models.Model):
     overview = models.TextField(blank=True)
     release_date = models.DateTimeField(null=True)
     tmdb_id = models.IntegerField(null=True)
-    genre = models.ManyToManyField("Genre", related_name="genres", blank=True)
+    genre = models.ManyToManyField("Genre", related_name="movies", blank=True)
     role = models.CharField(max_length=128, blank=True)
     media_type = models.CharField(max_length=2, choices=MEDIA_TYPE_CHOICES, blank=True)
     poster_path = models.URLField(max_length=200, blank=True)
@@ -24,10 +24,17 @@ class Title(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_genres(self):
+        return ", ".join([x.name for x in self.genre.all()])
 
 class Genre(models.Model):
     name = models.CharField(max_length=32)
     tmdb_id = models.IntegerField()
+    
 
     def __str__(self):
         return self.name
+    
+    def get_movies(self):
+        return ", ".join([x.title for x in self.movies.all()])
