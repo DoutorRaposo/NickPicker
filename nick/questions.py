50886c283776e568edb31d0e4a209a6d47a4b237
DoutorRaposo <rafaelvga93@gmail.com>
2024-02-07 03:22:03 -0300

    Quiz completed

--- nick/questions.py
+++ nick/questions.py
@@ -4,13 +4,14 @@ questions = {
     "questions": [
         {
             "Title": "How are you today?",
+            # first value should indicate the "feeling" of the gif
             "Options": [('gif1', "gif1"), ('gif2', "gif2"), ('gif3', "gif3")],
             "Select": "img"
         },
         {
             "Title": "Choose any genre you're interested in",
-            "Options": [(x.id, str(x)) for x in list(Genre.most_used())],
-            "Select": "or"
+            "Options": [(False, "Doesn't matter")] + [(x.id, str(x)) for x in list(Genre.most_used())],
+            "Select": "and"
         },
         {
             "Title": "Do you wish the movie to be from a specific decade?",
@@ -21,7 +22,7 @@ questions = {
                 ("90s", "1990's"),
                 ("80s","1980's"),
             ],
-            "Select": "and",
+            "Select": "or",
         },
         {
             "Title": "Do you wish to select prefered age ratings?",
@@ -31,12 +32,12 @@ questions = {
                 ("PG-13", "Teens 13+ (PG-13)"),
                 ("R", "Adults 18+ (NC-17, NR, R and Unrated)")
             ],
-            "Select": "and"
+            "Select": "or"
         },
         {
             "Title": "Select any other keyword you're interested in.",
             "Options": [(False, "Doesn't matter")] + [(x.id, f"'{str(x).title()}'") for x in list(Keyword.most_used())],
-            "Select": "or"
+            "Select": "and"
         },
     ]
 }
--- nick/static/nick/script.js
+++ nick/static/nick/script.js
@@ -2,68 +2,173 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  document.querySelector(".start-button").addEventListener('click', (e) => startQuiz(e));
+  document.querySelector(".start-button").addEventListener('click', (e) => fetchQuiz(e));
 
 });
 
 
-function startQuiz(e) {
+function fetchQuiz(e) {
   const appdiv = e.target.parentElement.parentElement;
   fetch('questions/')
     .then(response => response.json())
     .then(response => {
       document.querySelector(".index-welcome").style.display = "none"
-      mountQuestions(response, appdiv);
+      initializeQuiz(response, appdiv);
     })
 }
 
-function mountQuestions(questions, target) {
+function initializeQuiz(questions, target) {
+  const quizWrapper = initializeQuestions(questions);
+  target.append(quizWrapper)
+  const question_total = questions.questions.length;
+
+  const nextButtons = document.querySelectorAll(".next-btn")
+  nextButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      const current_question = e.target.parentElement;
+      const index = Number(current_question.dataset.question_id);
+      current_question.style.display = "none";
+      if (question_total > (index + 1)) {
+        document.querySelector(`#question-${index + 1}`).style.display = 'block';
+      }
+      else {
+        const answers_list = document.querySelectorAll(".selected");
+        let answers_obj = {}
+        for (let i = 0; i < answers_list.length; i++) {
+          if (answers_list[i].dataset.relation === "or" || answers_list[i].dataset.relation === "img") {
+            answers_obj[answers_list[i].dataset.question_id] = answers_list[i].dataset.answer_id
+          }
+          else if (answers_list[i].dataset.relation === "and") {
+            if (answers_obj[answers_list[i].dataset.question_id] == undefined) {
+              answers_obj[answers_list[i].dataset.question_id] = [answers_list[i].dataset.answer_id];
+            }
+            else {
+              answers_obj[answers_list[i].dataset.question_id].push(answers_list[i].dataset.answer_id);
+            }
+          }
+        }
+
+        for (let i = 0; i < question_total; i++) {
+          if (answers_obj[i] == undefined)
+          {
+            answers_obj[i] = "false";
+          }
+        }
+        // This is the object we will send in POST!!
+        console.log(answers_obj)
+      }
+    })
+  });
+
+  const prevButtons = document.querySelectorAll(".back-btn")
+  prevButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      const current_question = e.target.parentElement;
+      const index = Number(current_question.dataset.question_id);
+      current_question.style.display = "none";
+      if (0 <= (index - 1)) {
+        document.querySelector(`#question-${index - 1}`).style.display = 'block';
+      }
+    })
+  });
+  document.querySelector(`#question-0`).style.display = "block";
+}
+
+function initializeQuestions(questions) {
   const quizWrapper = document.createElement('div');
-  quizWrapper.className = "quiz-wrapper"
+  quizWrapper.className = "quiz-wrapper";
 
   questions.questions.forEach((question, index) => {
     const questionBox = document.createElement('div');
     questionBox.className = 'question-box';
     questionBox.id = `question-${index}`;
+    questionBox.dataset.question_id = `${index}`;
 
-    const titleWrapper = document.createElement('div')
-    titleWrapper.className = 'question-wrapper'
+    const titleWrapper = document.createElement('div');
+    titleWrapper.className = 'question-wrapper';
     const title = document.createElement('div');
     title.className = 'question-wrapper__question-title';
     title.innerHTML = `${index + 1}. ${question.Title}`;
-    titleWrapper.append(title)
+    titleWrapper.append(title);
 
-    console.log(question)
-    if (question.Select === "or") {
+    if (question.Select === "and") {
       const subtitle = document.createElement('div');
-      subtitle.className = "question-wrapper__question-subtitle"
+      subtitle.className = "question-wrapper__question-subtitle";
       subtitle.innerHTML = "Multiple answers are possible";
-      titleWrapper.append(subtitle)
+      titleWrapper.append(subtitle);
     }
-    questionBox.append(titleWrapper)
+    questionBox.append(titleWrapper);
 
-    const answers = document.createElement('ul')
-    answers.className = "option-box"
-    answers.dataset.relation = question.Select
-    question.Options.forEach(option => {
-      const li = document.createElement('li')
-      li.innerHTML = `${option[1]}`
-      li.dataset.answer_id = `${option[0]}`
-      li.dataset.question_id = `${index}`
-      li.className = 'option-box__li unselected'
-      li.addEventListener('click', () => {
-        if (li.className === 'option-box__li selected') {
-          li.className = 'option-box__li unselected'
+    const answers = document.createElement('ul');
+    answers.className = "option-box";
+    answers.dataset.relation = question.Select;
+    question.Options.forEach((option, option_index) => {
+      const li = document.createElement('li');
+      li.innerHTML = `${option[1]}`;
+      li.dataset.answer_id = `${option[0]}`;
+      li.dataset.question_id = `${index}`;
+      li.dataset.relation = question.Select;
+      li.className = 'option-box__li unselected';
+      li.id = `answer-question-${index}`
+      if (question.Select === "or" || question.Select === "img") {
+        li.addEventListener('click', () => {
+          const other_answers = document.querySelectorAll(`#${li.id}`)
+          other_answers.forEach(element => {
+            element.className = 'option-box__li unselected';
+          }
+
+          )
+          li.className = 'option-box__li selected';
+
+          document.querySelector(`#next-btn-${index}`).style.display = "block";
+        });
+      }
+      else if (question.Select === "and") {
+        li.addEventListener('click', () => {
+          if (option_index == 0) {
+            const other_answers = document.querySelectorAll(`#${li.id}`)
+            other_answers.forEach(element => {
+              element.className = 'option-box__li unselected';
+            })
+          }
+          else {
+            const other_answers = document.querySelectorAll(`#${li.id}`)[0].className = 'option-box__li unselected';
+          }
+
+          if (li.className === 'option-box__li selected') {
+            li.className = 'option-box__li unselected';
+          }
+          else {
+            li.className = 'option-box__li selected';
+          }
+
+
+          document.querySelector(`#next-btn-${index}`).style.display = "block";
         }
-        else {
-          li.className = 'option-box__li selected'
-        }
-      })
-      answers.append(li)
+        );
+      }
+      answers.append(li);
     });
     questionBox.append(answers);
 
+    if (index != 0) {
+      const backButton = document.createElement('div')
+      backButton.className = "back-btn";
+      backButton.id = `back-btn-${index}`;
+      backButton.innerHTML = "Back";
+      questionBox.append(backButton);
+    };
+
+    const nextButton = document.createElement('div');
+    nextButton.className = "next-btn";
+    nextButton.id = `next-btn-${index}`;
+    nextButton.innerHTML = "Next";
+    nextButton.style.display = "none";
+    nextButton.dataset.question_id = `${index}`
+
+    questionBox.append(nextButton)
+    questionBox.style.display = "none";
     quizWrapper.append(questionBox);
-  })
-  target.append(quizWrapper)
+  });
+  return quizWrapper;
 }
