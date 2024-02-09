ec5ce2d3269fb945917fd81d20ce6113276a9610
DoutorRaposo <rafaelvga93@gmail.com>
2024-02-09 11:36:35 -0300

    Two versions of script, just to remove comments in production one

--- nick/static/src-js/script.js
+++ nick/static/src-js/script.js
@@ -6,48 +6,55 @@ const transitionTime = 250;
 
 // When the content loads, the start button initializes the quiz
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".start-button").addEventListener('click', e => fetchQuiz(e));
+
+  document.querySelector(".start-button").addEventListener('click', (e) => fetchQuiz(e));
+
 });
 
 // This function gets the quiz info and starts the function that mounts the questions
 function fetchQuiz(e) {
   const appdiv = e.target.parentElement.parentElement;
-  fetch('questions/').then(response => response.json()).then(response => {
-    document.querySelector(".index-welcome").style.opacity = '0';
-    setTimeout(() => {
-      document.querySelector(".index-welcome").style.display = "none";
-    }, transitionTime);
-    initializeQuiz(response, appdiv);
-  });
+  fetch('questions/')
+    .then(response => response.json())
+    .then(response => {
+      document.querySelector(".index-welcome").style.opacity = '0';
+      setTimeout(() => {
+        document.querySelector(".index-welcome").style.display = "none";
+      }, transitionTime)
+      initializeQuiz(response, appdiv);
+    })
 }
 
 // This function mounts the questions and the functions of the next and back buttons
 function initializeQuiz(questions, target) {
   const quizWrapper = initializeQuestions(questions);
-  target.append(quizWrapper);
+  target.append(quizWrapper)
   const question_total = questions.questions.length;
+
   setNextButton(question_total);
+
   setBackButton();
 
   // This is the first question showing up!
   document.querySelector(`#question-0`).style.display = "block";
   setTimeout(() => {
     document.querySelector(`#question-0`).style.opacity = '1';
-  }, transitionTime);
+  }, transitionTime)
 }
 
 //This function sets up the back button to hide the current question and show the next one
 function setBackButton() {
   const prevButtons = document.querySelectorAll(".back-btn");
   prevButtons.forEach(button => {
-    button.addEventListener('click', e => {
+    button.addEventListener('click', (e) => {
       const current_question = e.target.parentElement.parentElement;
       const index = Number(current_question.dataset.question_id);
       current_question.style.opacity = '0';
       setTimeout(() => {
         current_question.style.display = "none";
       }, transitionTime);
-      if (0 <= index - 1) {
+
+      if (0 <= (index - 1)) {
         setTimeout(() => {
           document.querySelector(`#question-${index - 1}`).style.display = 'block';
           document.querySelector(`#question-${index - 1}`).style.opacity = '1';
@@ -61,14 +68,16 @@ function setBackButton() {
 function setNextButton(question_total) {
   const nextButtons = document.querySelectorAll(".next-btn");
   nextButtons.forEach(button => {
-    button.addEventListener('click', e => {
+    button.addEventListener('click', (e) => {
       const current_question = e.target.parentElement;
       const index = Number(current_question.dataset.question_id);
+
       current_question.style.opacity = '0';
       setTimeout(() => {
         current_question.style.display = "none";
       }, transitionTime);
-      if (question_total > index + 1) {
+
+      if (question_total > (index + 1)) {
         document.querySelector(`#question-${index + 1}`).style.display = "block";
         setTimeout(() => {
           document.querySelector(`#question-${index + 1}`).style.opacity = '1';
@@ -81,6 +90,7 @@ function setNextButton(question_total) {
     });
   });
 }
+
 function getResults(question_total) {
   const answers_list = document.querySelectorAll(".selected");
   let answers_obj = {};
@@ -88,10 +98,12 @@ function getResults(question_total) {
   for (let i = 0; i < answers_list.length; i++) {
     if (answers_list[i].dataset.relation === "xor" || answers_list[i].dataset.relation === "img") {
       answers_obj[answers_list[i].dataset.question_id] = answers_list[i].dataset.answer_id;
-    } else if (answers_list[i].dataset.relation === "and") {
+    }
+    else if (answers_list[i].dataset.relation === "and") {
       if (answers_obj[answers_list[i].dataset.question_id] == undefined) {
         answers_obj[answers_list[i].dataset.question_id] = [answers_list[i].dataset.answer_id];
-      } else {
+      }
+      else {
         answers_obj[answers_list[i].dataset.question_id].push(answers_list[i].dataset.answer_id);
       }
     }
@@ -110,11 +122,13 @@ function getResults(question_total) {
 function initializeQuestions(questions) {
   const quizWrapper = document.createElement('div');
   quizWrapper.className = "quiz-wrapper";
+
   questions.questions.forEach((question, index) => {
     const questionBox = document.createElement('div');
     questionBox.className = 'question-box';
     questionBox.id = `question-${index}`;
     questionBox.dataset.question_id = `${index}`;
+
     const titleWrapper = document.createElement('div');
     titleWrapper.className = 'question-wrapper';
     const title = document.createElement('div');
@@ -135,9 +149,11 @@ function initializeQuestions(questions) {
     const answers = document.createElement('ul');
     if (question.Options.length < 12) {
       answers.className = "option-box";
-    } else {
+    }
+    else {
       answers.className = "option-box big";
     }
+  
     answers.dataset.relation = question.Select;
     question.Options.forEach((option, option_index) => {
       const li = document.createElement('li');
@@ -146,7 +162,7 @@ function initializeQuestions(questions) {
       if (question.Select === "img") {
         const divGif = document.createElement('div');
         divGif.className = 'gif-container';
-        const imgGif = document.createElement('img');
+        const imgGif = document.createElement('img')
         imgGif.className = 'gif-container__img';
         imgGif.src = `${option[1]}`;
         const spanText = document.createElement('span');
@@ -154,18 +170,21 @@ function initializeQuestions(questions) {
         spanText.innerHTML = `${option[0]}`;
         divGif.append(imgGif, spanText);
         li.append(divGif);
-      } else {
-        const icon = document.createElement('i');
-        icon.className = "fa-regular fa-square";
+      }
+      else {
+        const icon = document.createElement('i')
+        icon.className = "fa-regular fa-square"
+
         const textSpan = document.createElement('span');
         textSpan.innerHTML = `${option[1]}`;
         li.append(icon, textSpan);
       }
+
       li.dataset.answer_id = `${option[0]}`;
       li.dataset.question_id = `${index}`;
       li.dataset.relation = question.Select;
       li.className = 'option-box__li unselected';
-      li.id = `answer-question-${index}`;
+      li.id = `answer-question-${index}`
       createAnswers(question, li, index, option_index);
       answers.append(li);
     });
@@ -173,29 +192,31 @@ function initializeQuestions(questions) {
 
     // Back button don't exist for the first question
     if (index != 0) {
-      const backButton = document.createElement('div');
+      const backButton = document.createElement('div')
       backButton.className = "back-btn-wrapper";
       backButton.id = `wrapper-back-btn-${index}`;
-      const spanButton = document.createElement('span');
+      const spanButton = document.createElement('span')
       spanButton.innerHTML = "< Back";
       spanButton.className = "back-btn";
       spanButton.id = `back-btn-${index}`;
       backButton.append(spanButton);
       questionBox.append(backButton);
-    }
-    ;
+    };
+
     const nextButton = document.createElement('div');
     nextButton.className = "next-btn";
     nextButton.id = `next-btn-${index}`;
     if (index + 1 === questions.questions.length) {
       nextButton.innerHTML = "Check Results";
-    } else {
+    }
+    else {
       nextButton.innerHTML = "Next";
     }
     nextButton.style.display = "none";
     nextButton.style.opacity = "0";
-    nextButton.dataset.question_id = `${index}`;
-    questionBox.append(nextButton);
+    nextButton.dataset.question_id = `${index}`
+
+    questionBox.append(nextButton)
     questionBox.style.display = "none";
     questionBox.style.opacity = '0';
     quizWrapper.append(questionBox);
@@ -205,6 +226,7 @@ function initializeQuestions(questions) {
 
 // Builds every answers for every question
 function createAnswers(question, li, index, option_index) {
+
   // For every type of question, a different path in building
   if (question.Select === "xor") {
     li.addEventListener('click', () => {
@@ -212,27 +234,35 @@ function createAnswers(question, li, index, option_index) {
       other_answers.forEach(element => {
         element.className = 'option-box__li unselected';
         element.querySelector('i').className = "fa-regular fa-square";
-      });
+      }
+
+      );
       li.className = 'option-box__li selected';
       li.querySelector('i').className = "fa-regular fa-square-check";
+
       setTimeout(() => {
         document.querySelector(`#next-btn-${index}`).style.display = "block";
         document.querySelector(`#next-btn-${index}`).style.opacity = '1';
       }, transitionTime / 2);
     });
-  } else if (question.Select === "img") {
+  }
+  else if (question.Select === "img") {
     li.addEventListener('click', () => {
       const other_answers = document.querySelectorAll(`#${li.id}`);
       other_answers.forEach(element => {
         element.className = 'option-box__li unselected';
-      });
+      }
+
+      );
       li.className = 'option-box__li selected';
+
       setTimeout(() => {
         document.querySelector(`#next-btn-${index}`).style.display = "block";
         document.querySelector(`#next-btn-${index}`).style.opacity = '1';
       }, transitionTime / 2);
     });
-  } else if (question.Select === "and") {
+  }
+  else if (question.Select === "and") {
     li.addEventListener('click', () => {
       if (option_index == 0) {
         const other_answers = document.querySelectorAll(`#${li.id}`);
@@ -240,21 +270,28 @@ function createAnswers(question, li, index, option_index) {
           element.className = 'option-box__li unselected';
           element.querySelector('i').className = "fa-regular fa-square";
         });
-      } else {
+      }
+      else {
         document.querySelectorAll(`#${li.id}`)[0].className = 'option-box__li unselected';
         document.querySelectorAll(`#${li.id}`)[0].querySelector('i').className = "fa-regular fa-square";
       }
+
       if (li.className === 'option-box__li selected') {
         li.className = 'option-box__li unselected';
         li.querySelector('i').className = "fa-regular fa-square";
-      } else {
+      }
+      else {
         li.className = 'option-box__li selected';
         li.querySelector('i').className = "fa-regular fa-square-check";
       }
+
       setTimeout(() => {
         document.querySelector(`#next-btn-${index}`).style.display = "block";
         document.querySelector(`#next-btn-${index}`).style.opacity = '1';
       }, transitionTime / 2);
-    });
+
+    }
+    );
   }
 }
+
